89a6fcee24752be1fe2b02dbc983eecd09f7e959
Faisal Bentouhami <bentouhami.faisal@gmail.com>
2024-09-25 16:15:56 +0200

    user interface enhanced for simulation results
    Took 1 hour 8 minutes

--- src/components/forms/SimulationResults.tsx
+++ src/components/forms/SimulationResults.tsx
@@ -1,20 +1,22 @@
 'use client';
 import {useRouter, useSearchParams} from 'next/navigation';
 import React, {useEffect, useState} from 'react';
-import {Button, Card, Row} from 'react-bootstrap';
+import {Button, Card, Col, Row} from 'react-bootstrap';
 import styles from './SimulationResults.module.css';
 import {SimulationResultsDto} from "@/app/utils/dtos";
 import {submitSimulation} from "@/app/utils/api";
-import LoginPromptModal from '@/components/LoginPromptModal'; // Assurez-vous que le chemin est correct
+import LoginPromptModal from '@/components/LoginPromptModal';
+import {CiCalculator2, CiCircleInfo} from "react-icons/ci";
+import {MdEmojiSymbols} from "react-icons/md";
+import {motion} from 'framer-motion';  // Import Framer Motion
 
 const SimulationResults = () => {
     const searchParams = useSearchParams();
     const router = useRouter();
     const [results, setResults] = useState<SimulationResultsDto | null>(null);
     const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
-    const [showLoginPrompt, setShowLoginPrompt] = useState(false); // État pour le modal
+    const [showLoginPrompt, setShowLoginPrompt] = useState(false);
 
-    // Effect to handle simulation results retrieval
     useEffect(() => {
         const data = searchParams.get('data');
         if (data) {
@@ -23,38 +25,31 @@ const SimulationResults = () => {
             const savedResults = localStorage.getItem('simulationResults');
             if (savedResults) {
                 setResults(JSON.parse(savedResults));
-                // localStorage.removeItem('simulationResults'); // Supprimer après récupération
             } else {
                 router.push('/simulation');
             }
         }
     }, [searchParams, router]);
 
-    // Separate effect to handle authentication check
     useEffect(() => {
         const checkAuthStatus = async () => {
             try {
                 const response = await fetch('/api/v1/auth/status');
                 const authStatus = await response.json();
                 setIsAuthenticated(authStatus.isAuthenticated);
-                console.log("Auth status:", authStatus.isAuthenticated);
             } catch (error) {
                 console.error("Erreur lors de la vérification de l'authentification", error);
                 setIsAuthenticated(false);
             }
         };
-
         checkAuthStatus();
-    }, []); // Dependency array empty to run this effect only once on mount
+    }, []);
 
     if (!results) {
         return <p className={styles.loading}>Loading...</p>;
     }
 
-    // Fonction de validation
     async function handleValidate() {
-
-
         if (isAuthenticated) {
             try {
                 if (results) {
@@ -65,58 +60,141 @@ const SimulationResults = () => {
                 console.error("Erreur lors de la soumission de la simulation", error);
             }
         } else {
-            // Sauvegarder les résultats dans localStorage avant d'afficher le modal de connexion
             localStorage.setItem('simulationResults', JSON.stringify(results));
-            setShowLoginPrompt(true); // Afficher le modal de connexion
+            setShowLoginPrompt(true);
         }
     }
 
-    // Fonction pour rediriger vers la page de login avec un paramètre de redirection
     const handleLoginRedirect = () => {
-        setShowLoginPrompt(false); // Fermer le modal
-        // Rediriger vers la page de login avec un paramètre `redirect`
+        setShowLoginPrompt(false);
         const redirectUrl = encodeURIComponent('/simulation/results');
         router.push(`/login?redirect=${redirectUrl}`);
     };
 
     function handleCancel() {
-        // supprimer les résultats de la simulation
         localStorage.removeItem('simulationResults');
         router.push('/simulation');
     }
 
     return (
-        <div className={`container mb-40 ${styles.container}`}>
+        <motion.div
+            className={`container mb-40 ${styles.container}`}
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+        >
             <h2 className={styles.heading}>Résultats de la Simulation</h2>
+
             <Row>
                 <Card className={styles.card}>
+                    <Card.Header className="text-center bg-blue-600 text-white p-5 rounded-3 mb-3">
+                        <motion.h2
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            transition={{ delay: 0.3 }}
+                        >
+                            Récapitulatif de votre envoi
+                        </motion.h2>
+                    </Card.Header>
+
                     <Card.Body>
-                        <h4>Informations de l&apos;envoi</h4>
-                        <p><strong>Pays de départ:</strong> {results.departureCountry}</p>
-                        <p><strong>Ville de départ:</strong> {results.departureCity}</p>
-                        <p><strong>Agence de départ:</strong> {results.departureAgency}</p>
-                        <p><strong>Pays de destination:</strong> {results.destinationCountry}</p>
-                        <p><strong>Ville de destination:</strong> {results.destinationCity}</p>
-                        <p><strong>Agence de destination:</strong> {results.destinationAgency}</p>
+                        {/* Informations de l'envoi */}
+                        <motion.div
+                            className="bg-blue-600 d-flex align-items-center mb-3 p-3 rounded"
+                            initial={{ opacity: 0, x: -50 }}
+                            animate={{ opacity: 1, x: 0 }}
+                            transition={{ delay: 0.5 }}
+                        >
+                            <CiCircleInfo className="size-5 mt-3 me-2" />
+                            <h4 className="text-white">Informations de l&apos;envoi</h4>
+                        </motion.div>
 
-                        <h4>Résultats des Colis</h4>
-                        {results.packages.map((pkg, index) => (
-                            <div key={index} className={styles['package-info']}>
-                                <p><strong>Colis {index + 1}:</strong></p>
-                                <p>Hauteur: {pkg.height} cm</p>
-                                <p>Largeur: {pkg.width} cm</p>
-                                <p>Longueur: {pkg.length} cm</p>
-                                <p>Poids: {pkg.weight} kg</p>
-                            </div>
-                        ))}
+                        <Row>
+                            <motion.div
+                                className=" p-5 rounded-3 shadow mb-3"
+                                initial={{ opacity: 0 }}
+                                animate={{ opacity: 1 }}
+                                transition={{ delay: 0.7 }}
+                            >
+                                <p><strong>Pays de départ:</strong> {results.departureCountry}</p>
+                                <p><strong>Ville de départ:</strong> {results.departureCity}</p>
+                                <p><strong>Agence de départ:</strong> {results.departureAgency}</p>
+                            </motion.div>
 
-                        <h4>Calculs</h4>
-                        <p><strong>Poids total:</strong> {results.totalWeight} kg</p>
-                        <p><strong>Volume total:</strong> {results.totalVolume} cm²</p>
-                        <p><strong>Prix total:</strong> {results.totalPrice} €</p>
-                        <p><strong>Date de départ:</strong> {new Date(results.departureDate).toLocaleDateString()}</p>
-                        <p><strong>Date d&apos;arrivée:</strong> {new Date(results.arrivalDate).toLocaleDateString()}
-                        </p>
+                            <motion.div
+                                className="p-5 rounded-3 shadow mb-3"
+                                initial={{ opacity: 0 }}
+                                animate={{ opacity: 1 }}
+                                transition={{ delay: 0.9 }}
+                            >
+                                <p><strong>Pays de destination:</strong> {results.destinationCountry}</p>
+                                <p><strong>Ville de destination:</strong> {results.destinationCity}</p>
+                                <p><strong>Agence de destination:</strong> {results.destinationAgency}</p>
+                            </motion.div>
+                        </Row>
+
+                        {/* Résultats des Colis */}
+                        <motion.div
+                            className="d-flex align-items-center mb-3 bg-green-600 text-white p-3 rounded"
+                            initial={{ opacity: 0, x: 50 }}
+                            animate={{ opacity: 1, x: 0 }}
+                            transition={{ delay: 1.1 }}
+                        >
+                            <MdEmojiSymbols className="size-5 mt-3 me-2" />
+                            <h4 className="text-white">Résultats des Colis</h4>
+                        </motion.div>
+
+                        <Row>
+                            {results.packages.map((pkg, index) => (
+                                <motion.div
+                                    key={index}
+                                    className="p-5 rounded-3 shadow mb-3"
+                                    initial={{ opacity: 0, scale: 0.9 }}
+                                    animate={{ opacity: 1, scale: 1 }}
+                                    transition={{ delay: 1.3 }}
+                                >
+                                    <p><strong>Colis {index + 1}:</strong></p>
+                                    <p>Hauteur: {pkg.height} cm</p>
+                                    <p>Largeur: {pkg.width} cm</p>
+                                    <p>Longueur: {pkg.length} cm</p>
+                                    <p>Poids: {pkg.weight} kg</p>
+                                </motion.div>
+                            ))}
+                        </Row>
+
+                        {/* Calculs */}
+                        <motion.div
+                            className="d-flex align-items-center mb-3 bg-orange-600 text-white p-3 rounded"
+                            initial={{ opacity: 0, x: -50 }}
+                            animate={{ opacity: 1, x: 0 }}
+                            transition={{ delay: 1.5 }}
+                        >
+                            <CiCalculator2 className="size-5 mt-3 me-2" />
+                            <h4 className="text-white">Calculs</h4>
+                        </motion.div>
+
+                        <Row>
+                            <motion.div
+                                className=" p-5 rounded-3 shadow mb-3"
+                                initial={{ opacity: 0 }}
+                                animate={{ opacity: 1 }}
+                                transition={{ delay: 1.7 }}
+                            >
+                                <p><strong>Poids total:</strong> {results.totalWeight} kg</p>
+                                <p><strong>Volume total:</strong> {results.totalVolume} cm²</p>
+                            </motion.div>
+
+                            <motion.div
+                                className="p-5 rounded-3 shadow mb-3"
+                                initial={{ opacity: 0 }}
+                                animate={{ opacity: 1 }}
+                                transition={{ delay: 1.9 }}
+                            >
+                                <p><strong>Prix total:</strong> {results.totalPrice} €</p>
+                                <p><strong>Date de départ:</strong> {new Date(results.departureDate).toLocaleDateString()}</p>
+                                <p><strong>Date d&apos;arrivée:</strong> {new Date(results.arrivalDate).toLocaleDateString()}</p>
+                            </motion.div>
+                        </Row>
                     </Card.Body>
                 </Card>
             </Row>
@@ -127,16 +205,17 @@ const SimulationResults = () => {
                 </Button>
 
                 <Button className="m-5" title="Annuler l&apos;envoi, et revenir à la page de simulation"
-                        onClick={handleCancel} type="button" variant="secondary">Annuler</Button>
+                        onClick={handleCancel} type="button" variant="secondary">
+                    Annuler
+                </Button>
             </div>
 
-            {/* Ajouter le LoginPromptModal */}
             <LoginPromptModal
                 show={showLoginPrompt}
                 handleClose={() => setShowLoginPrompt(false)}
                 handleLoginRedirect={handleLoginRedirect}
             />
-        </div>
+        </motion.div>
     );
 };
 
