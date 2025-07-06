36de1fb558d937d36eeff896130755923bc914a3
Bentouhami Faisal <bentouhami.faisal@gmail.com>
2025-07-06 21:15:11 +0200

    4th v0 sulution to fixe vercel redirect url after login success update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -125,13 +125,9 @@ export async function middleware(req: NextRequest) {
 // Configuration to apply middleware only to specific routes
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
-    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+    "/",
+    "/admin/:path*",
+    "/api/users/profile/:path*",
+    "/client/:path*",
   ],
 };
